fd00c69c2cd186b5650f14363f5eee90bf33d8de
HV <qvu@intracen.org>
2024-11-26 11:57:50 +0100

    Remove wrong lucia helper file

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,4 +1,3 @@
 import { i18n } from '$lib/i18n.js';
-import { auth } from '$lib/lucia.js'; // Import lucia-auth
 
 export const reroute = i18n.reroute();
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,19 +2,10 @@
 	import { i18n } from '$lib/i18n.js';
 	import { ParaglideJS } from '@inlang/paraglide-sveltekit';
 	import '../app.css';
-	import { onMount } from 'svelte';
-	import { page } from '$app/stores';
-	import { auth } from '$lib/lucia.js';
 
 	let { data, children } = $props<{ user: any; children: any }>();
 	let user = data.user;
 
-	onMount(async () => {
-		const session = await auth.getSession();
-		if (session) {
-			user = session.user;
-		}
-	});
 </script>
 
 <div class="layout">
